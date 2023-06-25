685f4559529eb612c82b85fd534b45e589221ae0
Alexey Ryzhkov <aler32@mail.ru>
2023-06-25 18:19:04 +0300

    lesson_3

--- alex_framework/templator.py
+++ alex_framework/templator.py
@@ -1,9 +1,17 @@
-from jinja2 import Template
-from os.path import join
+# from jinja2 import Template
+# from os.path import join
+from jinja2 import FileSystemLoader
+from jinja2.environment import Environment
 
 
-def render(templ_name, folder='templates', **kwargs):
-    file_path = join(folder, templ_name)
-    with open(file_path, encoding='utf-8') as f:
-        templ = Template(f.read())
-    return templ.render(**kwargs)
+def render(template_name, folder='templates', **kwargs):
+    env = Environment()
+    env.loader = FileSystemLoader(folder)
+    template = env.get_template(template_name)
+    return template.render(**kwargs)
+
+# def render(templ_name, folder='templates', **kwargs):
+#     file_path = join(folder, templ_name)
+#     with open(file_path, encoding='utf-8') as f:
+#         templ = Template(f.read())
+#     return templ.render(**kwargs)
